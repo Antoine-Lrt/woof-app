dbe9de37ce8c0bf0de80b6f2a681066af041334b
Antoine Laurent <antoine@macbook-pro-de-antoine.home>
2022-09-06 15:23:50 +0200

    First commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {COLORS, FONTSIZE, FONTWEIGHT} from './src/constants/GLOBAL'
+import CustomButton from './src/components/CustomButton/CustomButton';
+import CustomTextFiled from './src/components/CustomTextFiled/CustomTextFiled'
 
+interface style {
+  fontSize: number
+  fonttWeight: string
+}
 export default function App() {
+
+  const buttonPressed = () => {
+    console.log("button pressed");
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* <CustomButton type='ACTIVE_PRIMARY' onPress={buttonPressed} text='Default' />
+      <CustomButton type='ACTIVE_SECONDARY' onPress={buttonPressed} text='Default' />
+      <CustomButton type='DISABLED' onPress={buttonPressed} text='Default' />
+      <CustomButton type='TEXT_BUTTON_PRIMARY' onPress={buttonPressed} text='Default' />
+      <CustomButton type='TEXT_BUTTON_SECONDARY' onPress={buttonPressed} text='Default' />
+      <CustomButton size='SMALL' type='ACTIVE_PRIMARY' onPress={buttonPressed} text='Default' />
+      <CustomButton size='SMALL' type='ACTIVE_SECONDARY' onPress={buttonPressed} text='Default' />
+      <CustomButton size='SMALL' type='DISABLED' onPress={buttonPressed} text='Default' />
+      <CustomButton size='SMALL' type='TEXT_BUTTON_PRIMARY' onPress={buttonPressed} text='Default' />
+      <CustomButton size='SMALL' type='TEXT_BUTTON_SECONDARY' onPress={buttonPressed} text='Default' /> */}
+      <CustomTextFiled />
     </View>
   );
 }
@@ -13,7 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.BACKGROUND_COLORS_FIRST,
     alignItems: 'center',
     justifyContent: 'center',
   },
